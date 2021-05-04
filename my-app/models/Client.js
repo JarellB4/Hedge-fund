f0c5c1d8527d17e33d8a510757c04c513dfb969a
Jody Brzovski <jody.brzovski@gmail.com>
2021-05-04 11:17:45 -0400

    Create Client Model

---
+++ my-app/models/Client.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+
+const ClientSchema = new Schema({
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    unique: false
+  },
+  firstName: {
+    type: String,
+    unique: false
+  },
+  lastName: {
+    type: String,
+    unique: false
+  },
+  streetAddress1: {
+    type: String,
+    unique: false
+  },
+  streetAddress2: {
+    type: String,
+    unique: false
+  },
+  city: {
+    type: String,
+    unique: false
+  },
+  state: {
+    type: String,
+    unique: false
+  },
+  zip: {
+    type: Number,
+    unique: false,
+  },
+  location: {
+    type: "Point",
+    coordinates: [],
+  },  
+  job: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Job"
+    }
+  ]
+});
+
+const Client = mongoose.model("Client", ClientSchema);
+
+module.exports = Client;

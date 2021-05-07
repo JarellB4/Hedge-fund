15031f7dc76b35ba88d0745ef30b3c3510048457
Jody Brzovski <jody.brzovski@gmail.com>
2021-05-07 12:08:51 -0400

    working on job controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,59 @@
 const db = require("../models");
 
 module.exports = {
+  contractorFindAllJobs: function(req, res) {
+    db.Job
+      .find({})
+      .sort({ date: -1 })
+      .then(dbModel => {
+        console.log(dbModel)  
+        res.json(dbModel);
+      })      
+      .catch(err => res.status(422).json(err));
+  },
+  contractorUpdateJobQuote: function(req, res) {
+    db.Job
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  contractorDeleteJobQuote: function(req, res) {
+    db.Job
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  contractorCreateJobQuote: function(req, res) {
+    db.Job
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  clientFindAllJobs: function(req, res) {
+    db.Job
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  clientUpdateJob: function(req, res) {
+    db.Job
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  clientDeleteJob: function(req, res) {
+    db.Job
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  clientCreateJob: function(req, res) {
+    db.Job
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
 };

15904c6c2f5f73a41131b0dda0191b164c4056f6
Jody Brzovski <jody.brzovski@gmail.com>
2021-05-07 11:37:38 -0400

    Update contractorController.js

--- controllers/contractorController.js
+++ controllers/contractorController.js
@@ -1,5 +1,46 @@
 const db = require("../models");
 
-// Defining methods for the booksController
 module.exports = {
+  findAll: function(req, res) {
+    db.Contractor
+      .find({})
+      .populate({ 
+        path: "jobs",
+      })
+      .sort({ date: -1 })
+      .then(dbModel => {
+        console.log(dbModel)  
+        res.json(dbModel);
+      })      
+      .catch(err => res.status(422).json(err));
+  },
+
+  findById: function(req, res) {
+    db.Contractor
+      .findById(req.params.id)
+      .populate({ 
+        path: "jobs",
+      })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Contractor
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Contractor
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Contractor
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
 };

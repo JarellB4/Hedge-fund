add76005b747b64e11190883862bc94e1d321430
Jody Brzovski <jody.brzovski@gmail.com>
2021-05-07 12:01:38 -0400

    create job routes

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -2,15 +2,23 @@ const router = require("express").Router();
 const jobsController = require("../../controllers/jobController");
 
 // Matches with "/api/jobs"
-router.route("/")
+// router.route("/")
 //   .get(jobsController.findAll)
-//   .post(jobsController.create);
 
-// // Matches with "/api/jobs/:id"
-// router.route("/:id")
-//   .get(jobsController.findById)
-//   .put(jobsController.update)
-//   .delete(jobsController.remove)
-//   .post(jobsController.createQuote);
+// Matches with "/api/jobs/client/:id" 
+ //pass in client id
+router.route("/client/:id")
+  .get(jobsController.clientFindAllJobs)
+  .put(jobsController.clientUpdateJob)
+  .delete(jobsController.clientDeleteJob)
+  .post(jobsController.clientCreateJob);
+
+// Matches with "/api/jobs/contractor/:id"
+//pass in contractor id
+router.route("/contractor/:id")
+  .get(jobsController.contractorFindAllJobs)  //return all jobs with quotes that belong to the contractor
+  .put(jobsController.contractorUpdateJobQuote)  //update a quote
+  .delete(jobsController.contractorDeleteJobQuote)  //delete a quote on a job
+  .post(jobsController.contractorCreateJobQuote); //create a quote for a job
 
 module.exports = router;

dd186261baecb31947b72163e78b7d2ea8fd223b
jarell Boone <jarellboone4@gmail.com>
2021-05-08 09:46:57 -0400

    Updated routes defined for sign up and landing pages

--- project/src/App.js
+++ project/src/App.js
@@ -4,12 +4,18 @@ import {Route, BrowserRouter as Router} from 'react-router-dom'
 import Dashboard from './screens/Dashboard'
 import SignUp from './screens/Signup';
 import ContractorSignUp from './screens/ContractorSignup'
+import ContractorDashboard from './screens/ContractorDashboard'
+import SignIn from './screens/SignIn'
+import ContractorSignIn from './screens/ContractorSignIn'
 function App() {
   return (
     <Router>
     <Route path= '/' exact component={SignUp} />
+    <Route path= '/SignIn' exact component={SignIn} />
     <Route path= '/dashboard'  component={Dashboard} />
-    <Route path= '/dashboard/ContractorSignup' component={ ContractorSignUp} />
+    <Route path= '/ContractorSignup'  component={ ContractorSignUp} />
+    <Route path= '/ContractorDashboard'  component={ ContractorDashboard} />
+    <Route path= '/ContractorSignIn'  component={ ContractorSignIn} />
    
 
 

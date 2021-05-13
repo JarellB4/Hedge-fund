944c8ea7a6ebfaa64e228dbc373b0eee451f7d48
Jody Brzovski <jody.brzovski@gmail.com>
2021-05-13 14:54:23 -0400

    Working on Contractor Context

---
+++ client/src/components/ContracotrQuoteCard/ContracotrQuoteCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useContractorContext } from "../../utils/ContractorContext";
+import "./style.css";
+
+const CustomerCard = (props) => {
+  const [ContractorState, dispatch] = useContractorContext();
+  
+  return (
+  );
+};
+
+export default CustomerCard;
